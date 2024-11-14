42a09664dd668ce379d85c562493c6e2f30ef031
Mischa <mischaaaquisumbing@gmail.com>
2024-11-14 16:39:53 +1100

    Refactor Invitation Email Notification

--- chipin/models.py
+++ chipin/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Group(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admin_groups')
+    members = models.ManyToManyField(User, related_name='group_memberships', blank=True)
+    invited_users = models.ManyToManyField(User, related_name='pending_invitations', blank=True)
 
     def __str__(self):
         return self.name
